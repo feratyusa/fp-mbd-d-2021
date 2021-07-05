33fd6351677a73e136fe6403086bd967863659c9
feratyusa <prabuega55@gmail.com>
2021-07-05 23:26:06 +0700

    tambah auto archive

---
+++ triggers/auto_archive_story.sql
@@ -0,0 +1,15 @@
+-- Fungsi Trigger Archive Story yang dibuat melebihi 24 jam dari current_timestamp
+CREATE OR REPLACE FUNCTION auto_archive_story() RETURNS TRIGGER AS $auto_archive_stroy$
+	BEGIN
+		IF (current_timestamp - NEW.date_updated <= interval '24 hour') THEN
+			INSERT INTO instagram.archive_story(
+				user_id, arcs_caption, arcs_size, date_created, date_updated)
+				VALUES (NEW.user_id, NEW.arcs_caption, NEW.arcs_size, NEW.date_created, NEW.date_updated);
+		END IF;
+		RETURN NEW;
+	END;
+$auto_archive_stroy$ LANGUAGE plpgsql;
+
+-- Trigger Archive Story
+CREATE TRIGGER auto_archive_story AFTER INSERT OR UPDATE ON instagram.story
+FOR EACH ROW EXECUTE FUNCTION auto_archive_story();
