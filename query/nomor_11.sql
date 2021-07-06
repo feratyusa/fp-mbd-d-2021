e184ffaf57ec3a124c8eb9b845d1570312b18c3c
feratyusa <prabuega55@gmail.com>
2021-07-06 11:03:40 +0700

    revisi nomor 11

--- query/nomor_11.sql
+++ query/nomor_11.sql
@@ -1,5 +1,5 @@
 -- Mengampilkan user follower
 SELECT u.user_username, uf.user_username
-FROM instagram.user u INNER JOIN instagram.user_following fu ON u.user_id = fu.user_id
+FROM instagram.user u INNER JOIN instagram.user_followers fu ON u.user_id = fu.user_id
 	INNER JOIN instagram.user uf ON fu.follower_id = uf.user_id
 WHERE u.user_username LIKE 'J%' AND uf.user_username = 'F%'
